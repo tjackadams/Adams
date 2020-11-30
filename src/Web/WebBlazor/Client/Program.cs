d4420592fc7200e5e8929c9058836be76f170a7a
Thomas Adams <tjackadams@itadams.co.uk>
2020-11-30 15:17:58 +0000

    remove typed client in favour of named client.

--- src/Web/WebBlazor/Client/Program.cs
+++ src/Web/WebBlazor/Client/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BlazorStrap;
@@ -9,29 +10,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
 using Polly.Extensions.Http;
-using WebBlazor.Client.HttpClients;
+using WebBlazor.Client.Infrastructure.HttpClients;
 
 namespace WebBlazor.Client
 {
     public class Program
-    {
+    { 
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddHttpClient(Infrastructure.HttpClients.HttpClients.SmokingClient, client =>
-            {
-                client.BaseAddress = new Uri(builder.Configuration["SmokingUrl"]);
-            })                
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>()
-                .AddPolicyHandler(GetRetryPolicy());
-
-            builder.Services.AddHttpClient<ISmokingHttpClient, SmokingHttpClient>(client =>
-            {
-                client.BaseAddress = new Uri(builder.Configuration["SmokingUrl"]);
-            })
+            builder.Services.AddScoped(
+                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddHttpClient(HttpClients.SmokingClient,
+                    client => { client.BaseAddress = new Uri(builder.Configuration["SmokingUrl"]); })
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>()
                 .AddPolicyHandler(GetRetryPolicy());
 
@@ -39,7 +32,7 @@ namespace WebBlazor.Client
             {
                 // Configure your authentication provider options here.
                 // For more information, see https://aka.ms/blazor-standalone-auth
-                builder.Configuration.Bind("Local", options.ProviderOptions); 
+                builder.Configuration.Bind("Local", options.ProviderOptions);
             });
 
             builder.Services.AddBootstrapCss();
@@ -53,7 +46,7 @@ namespace WebBlazor.Client
         {
             return HttpPolicyExtensions
                 .HandleTransientHttpError()
-                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
                 .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
     }
