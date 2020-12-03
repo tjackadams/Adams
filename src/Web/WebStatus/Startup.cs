65a157dda5c568c40609e0d5105c6bee707745aa
Thomas Adams <tjackadams@itadams.co.uk>
2020-12-03 15:06:20 +0000

    remove unused files

--- src/Web/WebStatus/Startup.cs
+++ src/Web/WebStatus/Startup.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Hosting;
 
 namespace WebStatus
 {
@@ -20,56 +16,15 @@ namespace WebStatus
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
-            services.AddOptions();
-            services.AddHealthChecks()
-                .AddCheck("self", () => HealthCheckResult.Healthy());
-
             services.AddHealthChecksUI()
                 .AddInMemoryStorage();
-            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            var pathBase = Configuration["PATH_BASE"];
-            if (!string.IsNullOrEmpty(pathBase))
-            {
-                app.UsePathBase(pathBase);
-            }
-
-            app.UseHealthChecksUI(config =>
-            {
-                config.ResourcesPath = string.IsNullOrEmpty(pathBase) ? "/ui/resources" : $"{pathBase}/ui/resources";
-                config.UIPath = "/hc-ui";
-            });
-
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-                endpoints.MapHealthChecks("/liveness", new HealthCheckOptions
-                {
-                    Predicate = r => r.Name.Contains("self")
-                });
-            });
+            app
+                .UseRouting()
+                .UseEndpoints(config => { config.MapHealthChecksUI(); });
         }
     }
 }
