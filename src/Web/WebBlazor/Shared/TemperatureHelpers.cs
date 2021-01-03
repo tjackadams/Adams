4905a0badab2a6890ba2cff77f1f023013460161
Thomas Adams <tjackadams@itadams.co.uk>
2021-01-03 15:11:48 +0000

    add probe temperature to the timeline card component.

---
+++ src/Web/WebBlazor/Shared/TemperatureHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebBlazor.Shared
+{
+    public static class TemperatureHelpers
+    {
+        public static double? ToCelsius(double? farenheit)
+        {
+            if (!farenheit.HasValue)
+            {
+                return null;
+            }
+
+            return Math.Round((double) (farenheit - 32) * 5 / 9);
+        }
+    }
+}
---
+++ src/Web/WebBlazor/Shared/TemperatureType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBlazor.Shared
+{
+    public enum TemperatureType
+    {
+        Grill,
+        Probe
+    }
+}
