844a8a2bc60918d981d1d349f3a1bd812bd2704b
Thomas Adams <tjackadams@itadams.co.uk>
2022-10-27 14:29:38 +0100

    setup for local debugging

--- Nexus.Hub/Nexus.Portal/Program.cs
+++ Nexus.Hub/Nexus.Portal/Program.cs
@@ -1,8 +1,27 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using Nexus.Portal;
 using Nexus.Todo;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var initialScopes = builder.Configuration["DownstreamApi:Scopes"]?.Split(' ') ?? builder.Configuration["MicrosoftGraph:Scopes"]?.Split(' ');
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+        .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+            .AddDownstreamWebApi("DownstreamApi",builder.Configuration.GetSection("DownstreamApi"))
+            .AddInMemoryTokenCaches();
+
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
 
 builder.Services.AddOptions<Settings>()
     .Bind(builder.Configuration)
@@ -10,7 +29,9 @@ builder.Services.AddOptions<Settings>()
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
+
 builder.Services.AddHttpClient<Client>((sp, client) =>
 {
     var settings = sp.GetRequiredService<IOptions<Settings>>();
@@ -25,11 +46,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
