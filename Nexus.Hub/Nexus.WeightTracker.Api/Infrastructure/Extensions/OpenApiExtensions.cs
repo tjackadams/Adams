ce0cfeab0081f78ccbd0f78903da7ea4b0c2e4d1
Thomas Adams <tjackadams@itadams.co.uk>
2022-11-13 10:50:32 +0000

    move nswag to extensions

---
+++ Nexus.Hub/Nexus.WeightTracker.Api/Infrastructure/Extensions/OpenApiExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Nexus.WeightTracker.Api.Domain;
+using Nexus.WeightTracker.Api.Infrastructure.NSwag;
+using NJsonSchema;
+using NJsonSchema.Generation.TypeMappers;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+
+namespace Nexus.WeightTracker.Api.Infrastructure.Extensions;
+
+public static class OpenApiExtensions
+{
+    public static WebApplicationBuilder AddOpenApi(this WebApplicationBuilder builder)
+    {
+        var swagger = builder.Configuration.GetRequiredSection("Swagger");
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddOpenApiDocument(document =>
+        {
+            document.Title = "Nexus Weight Tracking API";
+
+            document.SchemaNameGenerator = new NexusSchemaNameGenerator();
+
+            document.TypeMappers.Add(new PrimitiveTypeMapper(typeof(ClientId), schema =>
+            {
+                schema.Type = JsonObjectType.Integer;
+            }));
+
+            document.TypeMappers.Add(new PrimitiveTypeMapper(typeof(ClientMetricId), schema =>
+            {
+                schema.Type = JsonObjectType.Integer;
+            }));
+
+            document.AddSecurity(JwtBearerDefaults.AuthenticationScheme, Enumerable.Empty<string>(),
+                new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.OAuth2,
+                    Flow = OpenApiOAuth2Flow.AccessCode,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        AuthorizationCode = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = $"{swagger.GetValue<string>("Instance")}{swagger.GetValue<string>("TenantId")}/oauth2/v2.0/authorize",
+                            TokenUrl = $"{swagger.GetValue<string>("Instance")}{swagger.GetValue<string>("TenantId")}/oauth2/v2.0/token",
+                            Scopes = { { "api://cef30c8d-dc02-4e0f-aa61-52155ec9a9a6/nexus.weighttracker", "Nexus.WeightTracker" } }
+                        }
+                    }
+                });
+
+            document.OperationProcessors.Add(
+                new AspNetCoreOperationSecurityScopeProcessor(JwtBearerDefaults.AuthenticationScheme)
+            );
+        });
+
+        return builder;
+    }
+}
--- Nexus.Hub/Nexus.WeightTracker.Api/Program.cs
+++ Nexus.Hub/Nexus.WeightTracker.Api/Program.cs
@@ -32,45 +32,7 @@ builder.Services.AddAuthorization(options =>
 });
 
 builder.AddOpenTelemetry();
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddOpenApiDocument(document =>
-{
-    document.Title = "Nexus Weight Tracking API";
-
-    document.SchemaNameGenerator = new NexusSchemaNameGenerator();
-
-    document.TypeMappers.Add(new PrimitiveTypeMapper(typeof(ClientId), schema =>
-    {
-        schema.Type = JsonObjectType.Integer;
-    }));
-
-    document.TypeMappers.Add(new PrimitiveTypeMapper(typeof(ClientMetricId), schema =>
-    {
-        schema.Type = JsonObjectType.Integer;
-    }));
-
-    document.AddSecurity(JwtBearerDefaults.AuthenticationScheme, Enumerable.Empty<string>(),
-        new OpenApiSecurityScheme
-        {
-            Type = OpenApiSecuritySchemeType.OAuth2,
-            Flow = OpenApiOAuth2Flow.AccessCode,
-            Flows = new OpenApiOAuthFlows
-            {
-                AuthorizationCode = new OpenApiOAuthFlow
-                {
-                    AuthorizationUrl = $"{swagger.GetValue<string>("Instance")}{swagger.GetValue<string>("TenantId")}/oauth2/v2.0/authorize",
-                    TokenUrl = $"{swagger.GetValue<string>("Instance")}{swagger.GetValue<string>("TenantId")}/oauth2/v2.0/token",
-                    Scopes = { { "api://cef30c8d-dc02-4e0f-aa61-52155ec9a9a6/nexus.weighttracker", "Nexus.WeightTracker" } }
-                }
-            }
-        });
-
-    document.OperationProcessors.Add(
-        new AspNetCoreOperationSecurityScopeProcessor(JwtBearerDefaults.AuthenticationScheme)
-    );
-});
+builder.AddOpenApi();
 
 builder.Services.AddDbContext<WeightDbContext>(options =>
 {
