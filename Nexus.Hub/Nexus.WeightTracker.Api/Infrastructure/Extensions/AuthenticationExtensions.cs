8409bdc434aeea2e18115c7d07804baa384d8451
Thomas Adams <tjackadams@itadams.co.uk>
2022-11-13 10:52:08 +0000

    move auth to extensions

---
+++ Nexus.Hub/Nexus.WeightTracker.Api/Infrastructure/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+namespace Nexus.WeightTracker.Api.Infrastructure.Extensions;
+
+public static class AuthenticationExtensions
+{
+    public static WebApplicationBuilder AddAuth(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy("Reader", policy => policy.RequireRole("WeightTracker.Read"));
+            options.AddPolicy("Writer", policy => policy.RequireRole("WeightTracker.Write"));
+        });
+
+        return builder;
+    }
+}
--- Nexus.Hub/Nexus.WeightTracker.Api/Program.cs
+++ Nexus.Hub/Nexus.WeightTracker.Api/Program.cs
@@ -16,15 +16,7 @@ builder.Services.AddProblemDetails();
 
 var swagger = builder.Configuration.GetRequiredSection("Swagger");
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("Reader", policy => policy.RequireRole("WeightTracker.Read"));
-    options.AddPolicy("Writer", policy => policy.RequireRole("WeightTracker.Write"));
-});
-
+builder.AddAuth();
 builder.AddOpenTelemetry();
 builder.AddOpenApi();
 
