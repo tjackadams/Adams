3f5cf30b7d668cf64a5a38084a8f5223392db282
Thomas Adams <tjackadams@itadams.co.uk>
2022-11-20 13:17:31 +0000

    update authorization policy names

--- Nexus.Hub/Nexus.WeightTracker.Api/Infrastructure/Authorization/AuthorizationPolicyNames.cs
+++ Nexus.Hub/Nexus.WeightTracker.Api/Infrastructure/Authorization/AuthorizationPolicyNames.cs
@@ -1,6 +1,6 @@
 ﻿namespace Nexus.WeightTracker.Api.Infrastructure.Authorization;
 
-public class AuthorizationPolicy
+public class AuthorizationPolicyNames
 {
     public const string Reader = nameof(Reader);
     public const string Writer = nameof(Writer);
